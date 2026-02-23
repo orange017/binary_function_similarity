916b8b29c6fb487771d09209a5bc2630d800701f
orange017 <orange017@mail.ru>
2026-02-23 23:48:07 +0300

    Update IDA_acfg_disasm plugin (IDA 7.7 support)

--- IDA_scripts/IDA_acfg_disasm/IDA_acfg_disasm.py
+++ IDA_scripts/IDA_acfg_disasm/IDA_acfg_disasm.py
@@ -36,6 +36,7 @@
 import base64
 import idaapi
 import idc
+import ida_pro
 import json
 import ntpath
 import os
@@ -205,7 +206,7 @@ def get_basic_blocks(fva):
 
 def get_bb_disasm(bb, md, prefix):
     """Return the (nomalized) disassembly for a BasicBlock."""
-    b64_bytes = base64.b64encode(idc.get_bytes(bb.va, bb.size))
+    b64_bytes = base64.b64encode(idc.get_bytes(bb.va, bb.size)).decode()
     bb_heads, bb_mnems, bb_disasm, bb_norm = \
         capstone_disassembly(md, bb.va, bb.size, prefix)
     return b64_bytes, bb_heads, bb_mnems, bb_disasm, bb_norm
@@ -222,7 +223,7 @@ def run_acfg_disasm(idb_path, fva_list, output_dir):
     output_dict = dict()
     output_dict[idb_path] = dict()
 
-    procname = idaapi.get_inf_structure().procName.lower()
+    procname = idaapi.get_inf_structure().procname.lower()
     bitness = get_bitness()
     output_dict[idb_path]['arch'] = convert_procname_to_str(procname, bitness)
     md, prefix = initialize_capstone(procname, bitness)
@@ -280,12 +281,12 @@ def run_acfg_disasm(idb_path, fva_list, output_dir):
 if __name__ == '__main__':
     if not idaapi.get_plugin_options("acfg_disasm"):
         print("[!] -Oacfg_disasm option is missing")
-        idc.Exit(1)
+        ida_pro.qexit(1)
 
     plugin_options = idaapi.get_plugin_options("acfg_disasm").split(":")
     if len(plugin_options) != 3:
         print("[!] -Oacfg_disasm:INPUT_JSON:IDB_PATH:OUTPUT_DIR is required")
-        idc.Exit(1)
+        ida_pro.qexit(1)
 
     input_json = plugin_options[0]
     idb_path = plugin_options[1]
@@ -296,10 +297,10 @@ if __name__ == '__main__':
 
     if idb_path not in selected_functions:
         print("[!] Error! IDB path (%s) not in %s" % (idb_path, input_json))
-        idc.Exit(1)
+        ida_pro.qexit(1)
 
     fva_list = selected_functions[idb_path]
     print("[D] Found %d addresses" % len(fva_list))
 
     run_acfg_disasm(idb_path, fva_list, output_dir)
-    idc.Exit(0)
+    ida_pro.qexit(0)
